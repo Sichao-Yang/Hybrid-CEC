ae18168d107a9f6c39a12dc8a89722c7612815c6
sichao-yang <13816901408@163.com>
2025-08-09 14:39:16 +0800

    doc

--- bitset.h
+++ bitset.h
@@ -2,46 +2,45 @@
 
 #ifndef BITSET_H_
 #define BITSET_H_
-#include <random>
-#include <limits>
 #include <iostream>
+#include <limits>
+#include <random>
 typedef unsigned long long ull;
 
 class Bitset {
-public: 
-    static const ull size_correcter = 1ull;
-    static const ull one_bit = 1ull;
-    static const ull zero_bit = 0ull;
-    int bits = 8 * sizeof(ull);
-    int m_size = 0, n = 0;
-    ull *array, hashval;
-    void print();
-    void hash() noexcept;
-    void allocate(int sz) noexcept;
-    void random() noexcept;
-    void free() noexcept;
-    void eqs(const Bitset& u, int s) noexcept;
-    void ands(const Bitset& u, const Bitset& v, int s, int s1, int s2) noexcept;
-    void xors(const Bitset& u, const Bitset& v, int s, int s1, int s2) noexcept;
+public:
+  static const ull size_correcter = 1ull;
+  static const ull one_bit = 1ull;
+  static const ull zero_bit = 0ull;
+  int bits = 8 * sizeof(ull); // 8*8=64, 也就是用一个ull可以存8个字节64个比特
+  int m_size = 0, n = 0;
+  ull *array, hashval;
+  void print();
+  void hash() noexcept;
+  void allocate(int sz) noexcept;
+  void random() noexcept;
+  void free() noexcept;
+  void eqs(const Bitset &u, int s) noexcept;
+  void ands(const Bitset &u, const Bitset &v, int s, int s1, int s2) noexcept;
+  void xors(const Bitset &u, const Bitset &v, int s, int s1, int s2) noexcept;
 
-    bool operator==(const Bitset& rhs) const noexcept;
+  bool operator==(const Bitset &rhs) const noexcept;
 
-    constexpr int size() const noexcept;
+  constexpr int size() const noexcept;
 
-    //******** Modifiers **********//
-    void set() noexcept;
-    Bitset& set(int);
-    void reset() noexcept;
-    Bitset& reset(int);
-    Bitset& flip() noexcept;
+  //******** Modifiers **********//
+  void set() noexcept;
+  Bitset &set(int);
+  void reset() noexcept;
+  Bitset &reset(int);
+  Bitset &flip() noexcept;
 
-    Bitset& operator=(const Bitset& other) noexcept;
-    Bitset operator~() const noexcept;
-    int operator[](int) noexcept;
+  Bitset &operator=(const Bitset &other) noexcept;
+  Bitset operator~() const noexcept;
+  int operator[](int) noexcept;
 
-    //********** Element access********//
-    bool all() const noexcept;
+  //********** Element access********//
+  bool all() const noexcept;
 };
-
 
 #endif
--- makefile
+++ makefile
@@ -4,16 +4,20 @@ OBJS = $(addsuffix .o, $(basename $(SRCS)))
 
 EXEC = hybrid_cec
 
+# Debug version with -g flag and kissat library
 LIBS = -g -lkissat -LhKis/build
-LIBS = -lkissat -LhKis/build
 
-CXXFLAGS=-Ofast -IhKis -mavx2 -std=c++17 -march=native
+# Debug flags: remove optimization, add debug symbols and warnings
+CXXFLAGS = -g -O0 -IhKis -std=c++17 -Wall -Wextra -DDEBUG
+
+# Optional: Add sanitizers for better debugging
+# CXXFLAGS += -fsanitize=address -fsanitize=undefined
 
 $(EXEC): $(OBJS)
 	$(CXX) -o $@ $^ $(CXXFLAGS) $(LIBS)
 
 %.o: %.cpp
-	$(CXX) -c $< -o $@ $(CXXFLAGS) $(LIBS)
+	$(CXX) -c $< -o $@ $(CXXFLAGS)
 
 clean:
 	rm -f $(OBJS) $(EXEC)
